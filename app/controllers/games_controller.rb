6c92f1be8d8d3a448e4ab9b0c32dfc679a50e1f3
Qiyun Dai <pianoquin@gmail.com>
2021-02-09 21:38:47 -0600

    had to go look at my old code for inspirations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,23 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
     def new
         @letters = []
-        10.times { @letters << ('A'..'Z').to_a.sample.to_s }
+        10.times { @letters << ('A'..'Z').to_a.sample }
     end
 
     def score
-        raise
+        provided = params[:letters].split("").select { |i| (i =~ /\W/).nil? }
+        dict = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{params[:word].upcase}").read)
+        letters = Array.new(params[:word].upcase.split(""))
+        matched = letters.map { |letter| !(/#{letter}/ =~ provided.split("").join).nil? ? provided.delete_at(/#{letter}/ =~ provided.join) : nil }
+        if dict["found"] && letters.join == matched.join
+            @result = "#{params[:word]} is an English word & is from the letters provided. Well Done!"
+        elsif dict["found"]
+            @result = "#{params[:word]} is an English word but is not from the letters provided. Boo..."
+        else
+            @result = "#{params[:word]} is not even an English word. Who are you trying to fool!?"
+        end
     end
 end
