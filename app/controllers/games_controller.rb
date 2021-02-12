d1061d89c52d5794d4490b86fe26b9ff6af2d1f8
Qiyun Dai <pianoquin@gmail.com>
2021-02-11 19:16:37 -0600

    the word appears in bold now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,16 +8,17 @@ class GamesController < ApplicationController
     end
 
     def score
+        @word = params[:word]
         provided = params[:letters].split("").select { |i| (i =~ /\W/).nil? }
         dict = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{params[:word].upcase}").read)
         letters = Array.new(params[:word].upcase.split(""))
         matched = letters.map { |letter| !(/#{letter}/ =~ provided.split("").join).nil? ? provided.delete_at(/#{letter}/ =~ provided.join) : nil }
         if dict["found"] && letters.join == matched.join
-            @result = "#{params[:word]} is an English word & is from the letters provided. Well Done!"
+            @result = " is an English word & is from the letters provided. Well Done!"
         elsif dict["found"]
-            @result = "#{params[:word]} is an English word but is not from the letters provided. Boo..."
+            @result = " is an English word but is not from the letters provided. Boo..."
         else
-            @result = "#{params[:word]} is not even an English word. Who are you trying to fool!?"
+            @result = " is not even an English word. Who are you trying to fool!?"
         end
     end
 end
